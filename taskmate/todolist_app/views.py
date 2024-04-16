e08520071be395a8785447c17a191a3c650a7c89
DhanukaSheyamPerera <dhanuka.sp@outlook.com>
2024-04-16 12:02:41 +0530

    form functionality fully implemented

--- taskmate/todolist_app/views.py
+++ taskmate/todolist_app/views.py
@@ -1,15 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import TaskList
+from .forms import TaskForm
+from django.contrib import messages
 
 # Create your views here.
 def todolist(request):
-    # return HttpResponse("Welcome to todolist")
-    all_tasks = TaskList.objects.all()
-    context = {
+    if request.method == "POST":
+        form = TaskForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+        messages.success(request, 'New task added!')
+        return redirect('todolist')
+    else:
+        # return HttpResponse("Welcome to todolist")
+        all_tasks = TaskList.objects.all()
+        context = {
         "welcome_todolist_text":"Welcome to Todolist app"
-    }
-    return render(request, 'todolist.html', {'all_tasks': all_tasks})
+        }
+        return render(request, 'todolist.html', {'all_tasks': all_tasks})
 
 def contact(request):
     context = {
