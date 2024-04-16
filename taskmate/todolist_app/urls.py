c1d1dd36b81b99dfc7ab54f4e33ebcc8f6e54cfd
DhanukaSheyamPerera <dhanuka.sp@outlook.com>
2024-04-16 16:39:49 +0530

    task status updating functionality added and page pagination partially added (not working)

--- taskmate/todolist_app/urls.py
+++ taskmate/todolist_app/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('', views.todolist, name='todolist'),
     path('delete/<task_id>', views.delete_task, name="deletetask"),
     path('edit/<task_id>', views.edit_task, name="edittask"),
+    path('complete/<task_id>', views.complete_task, name="completetask"),
+    path('pending/<task_id>', views.pending_task, name="pendingtask"),
     path('contact', views.contact, name='contact'),
     path('about', views.about, name='about')
 
--- taskmate/todolist_app/views.py
+++ taskmate/todolist_app/views.py
@@ -4,6 +4,7 @@ from .models import TaskList
 from .forms import TaskForm
 from django.contrib import messages
 
+
 # Create your views here.
 def todolist(request):
     if request.method == "POST":
@@ -16,15 +17,17 @@ def todolist(request):
         # return HttpResponse("Welcome to todolist")
         all_tasks = TaskList.objects.all()
         context = {
-        "welcome_todolist_text":"Welcome to Todolist app"
+            "welcome_todolist_text": "Welcome to Todolist app"
         }
         return render(request, 'todolist.html', {'all_tasks': all_tasks})
+
 
 def delete_task(request, task_id):
     task = TaskList.objects.get(pk=task_id)
     task.delete()
     messages.success(request, "Your task deleted successfully!")
     return redirect('todolist')
+
 
 def edit_task(request, task_id):
     task = TaskList.objects.get(pk=task_id)
@@ -37,14 +40,28 @@ def edit_task(request, task_id):
     else:
         return render(request, 'edittask.html', {'task_title': task.task, 'task_status': task.status})
 
+
+def complete_task(request, task_id):
+    task = TaskList.objects.get(pk=task_id)
+    task.status = True
+    task.save()
+    return redirect('todolist')
+
+def pending_task(request, task_id):
+    task = TaskList.objects.get(pk=task_id)
+    task.status = False
+    task.save()
+    return redirect('todolist')
+
 def contact(request):
     context = {
-        "welcome_contact_text":"Welcome to Contact Us Page"
+        "welcome_contact_text": "Welcome to Contact Us Page"
     }
     return render(request, 'contact.html', context)
 
+
 def about(request):
     context = {
-        "welcome_about_text":"Welcome to About Us Page"
+        "welcome_about_text": "Welcome to About Us Page"
     }
     return render(request, 'about.html', context)
