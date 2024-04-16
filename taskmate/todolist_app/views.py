e8492820ab1a60db4c1d0befb4af36766c1d5736
DhanukaSheyamPerera <dhanuka.sp@outlook.com>
2024-04-16 16:58:04 +0530

    fully functionning page pagination feature added

--- taskmate/todolist_app/views.py
+++ taskmate/todolist_app/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import TaskList
 from .forms import TaskForm
 from django.contrib import messages
+from django.core.paginator import Paginator
 
 
 # Create your views here.
@@ -16,9 +17,9 @@ def todolist(request):
     else:
         # return HttpResponse("Welcome to todolist")
         all_tasks = TaskList.objects.all()
-        context = {
-            "welcome_todolist_text": "Welcome to Todolist app"
-        }
+        paginator = Paginator(all_tasks, 5)
+        page = request.GET.get('pg')
+        all_tasks = paginator.get_page(page)
         return render(request, 'todolist.html', {'all_tasks': all_tasks})
 
 
